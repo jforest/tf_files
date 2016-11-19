79ccda3a38efd549ad213de11619600864362cb3
Joshua C. Forest <joshua@joshuacforest.com>
2016-11-19 17:10:14 -0500

    add correct netwok for web boxes

--- testing/base.tf
+++ testing/base.tf
@@ -1,3 +1,4 @@
+# Variables
 variable "creds" {
   description = "Path to the JSON file used to describe your account credentials"
 }
@@ -6,12 +7,18 @@ variable "region" {
   default = "us-east1"
 }
 
+
+
+# Google creds
 provider "google" {
   credentials = "${file(var.creds)}"
   project     = "${var.project}"
   region      = "${var.region}"
 }
 
+
+
+# Network and subnet configs here
 resource "google_compute_network" "test" {
   name       = "test"
   auto_create_subnetworks = "false"
@@ -31,7 +38,10 @@ resource "google_compute_subnetwork" "www" {
   region        = "us-east1"
 }
 
-resource "google_compute_address" "fe" {
+
+
+# Static IPs here
+resource "google_compute_address" "bastion" {
   name = "test-bastion-address"
 }
 
@@ -39,7 +49,10 @@ resource "google_compute_address" "www" {
   name = "test-www-address"
 }
 
-resource "google_compute_firewall" "test-ssh" {
+
+
+# Firewall rules here
+resource "google_compute_firewall" "bastion-ssh" {
   name    = "test-ssh"
   network = "${google_compute_network.test.name}"
   description = "Allow in ssh from joshes home"
@@ -81,30 +94,63 @@ resource "google_compute_firewall" "internal" {
   target_tags = ["backend"]
 }
 
+
+
+# Load balancer below this
+resource "google_compute_target_pool" "www" {
+  name = "test-www-target-pool"
+  instances = ["${google_compute_instance.www.*.self_link}"]
+  health_checks = ["${google_compute_http_health_check.http.name}"]
+}
+
+resource "google_compute_forwarding_rule" "http" {
+  name = "test-www-http-forwarding-rule"
+  target = "${google_compute_target_pool.www.self_link}"
+  ip_address = "${google_compute_address.www.address}"
+  port_range = "80-80"
+}
+
+resource "google_compute_forwarding_rule" "https" {
+  name = "test-www-https-forwarding-rule"
+  target = "${google_compute_target_pool.www.self_link}"
+  ip_address = "${google_compute_address.www.address}"
+  port_range = "443-443"
+}
+
+resource "google_compute_http_health_check" "http" {
+  name = "test-www-http-basic-check"
+  request_path = "/"
+  check_interval_sec = 1
+  healthy_threshold = 1
+  unhealthy_threshold = 10
+  timeout_sec = 1
+}
+
+
+
+# Instances go here
 resource "google_compute_instance" "test-bastion" {
   name         = "test-bastion"
-  machine_type = "n1-standard-1"
+  machine_type = "f1-micro"
   zone         = "us-east1-d"
 
   tags = ["bastion"]
 
   disk {
     image = "ubuntu-os-cloud/ubuntu-1604-lts"
-    size = 50
   }
 
   network_interface {
     subnetwork = "test-subnet1"
     access_config {
-      nat_ip = "${google_compute_address.fe.address}"
+      nat_ip = "${google_compute_address.bastion.address}"
     }
   }
 
   metadata {
     hostname = "test.foresj.net"
+    sshKeys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
   }
-
-  metadata_startup_script = "echo hi > /test.txt"
 
   scheduling {
     on_host_maintenance = "MIGRATE"
@@ -116,10 +162,54 @@ resource "google_compute_instance" "test-bastion" {
   }
 }
 
+resource "google_compute_instance" "www" {
+  count = 2
+  name = "test-www-${count.index}"
+  machine_type = "f1-micro"
+  zone = "us-east1-d"
+  tags = ["www-node","backend"]
+
+  disk {
+    image = "ubuntu-os-cloud/ubuntu-1604-lts"
+  }
+
+  network_interface {
+    subnetwork = "test-subnet2"
+    access_config {
+        # Ephemeral
+    }
+  }
+
+  scheduling {
+    on_host_maintenance = "MIGRATE"
+    automatic_restart = "true"
+  }
+
+  service_account {
+    scopes = ["compute-ro"]
+  }
+
+  metadata_startup_script = <<SCRIPT
+aptitude -y update
+#aptitude -y safe-upgrade
+aptitude install -y nginx
+service nginx start
+SCRIPT
+
+  metadata {
+    hostname = "www${count.index}.foresj.net"
+    sshKeys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+  }
+}
 
 
 
-
-output "public_ip" {
+output "www_public_ip" {
   value = "${google_compute_address.www.address}"
+}
+output "bastion_public_ip" {
+  value = "${google_compute_address.bastion.address}"
+}
+output "www_output" {
+  value = "${google_compute_instance.www.network_interface.0.address}"
 }
